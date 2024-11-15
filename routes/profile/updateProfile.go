6471e605b2f018f8e088e9a778bb20f4ae5c78e7
NucleoFusion <lakshit.singh.mail@gmail.com>
2024-11-15 09:40:23 +0530

    Adding Profile Update Route

--- routes/profile/updateProfile.go
+++ routes/profile/updateProfile.go
@@ -23,47 +23,76 @@ func (s *updateProfile) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	body := r.PostForm
 
+	key := r.URL.Query().Get("key")
+
 	profile, err := DecodeBody(&body)
 	if err != nil {
-		io.WriteString(w, err.Error())
+		if err.Error() != "email not provided" {
+			io.WriteString(w, err.Error())
+			return
+		}
 	}
 
-	data, _ := json.Marshal(profile)
+	profile.Key = key
 
-	// checkExists := make(chan bool, 1)
+	checkExists := make(chan bool, 1)
+	profChan := make(chan *statics.Profile, 1)
+	sendMerged := make(chan *statics.Profile, 1)
 
-	// go InsertIntoProfile(profile, s.Db, checkExists)
+	go UpdateIntoProfile(profile, checkExists, profChan, sendMerged, s.Db)
 
-	// exists := <-checkExists
+	exists := <-checkExists
+	if !exists {
+		io.WriteString(w, "user doesnt exists")
+		return
+	}
 
-	// if exists {
-	// io.WriteString(w, "user already exists")
-	// return
-	// }
+	prof := <-profChan
+	MergeProfiles(profile, prof)
+	sendMerged <- prof
+
+	data, _ := json.Marshal(prof)
 
 	io.Writer.Write(w, data)
 }
 
-func UpdateIntoProfile(profile *statics.Profile, db *sql.DB, exists chan bool) {
-	var (
-		name        = ReturnNULL(profile.Name)
-		email       = profile.Email
-		linkedin    = ReturnNULL(profile.LinkedIn)
-		description = ReturnNULL(profile.Description)
-		birthDate   = profile.BirthDate
-	)
+func UpdateIntoProfile(profile *statics.Profile, exists chan bool, profileChan chan *statics.Profile, getMerged chan *statics.Profile, db *sql.DB) {
+	res := db.QueryRow("SELECT * FROM profile WHERE key = $1", profile.Key)
+	prof := statics.Profile{}
 
-	res := db.QueryRow("SELECT * FROM profile WHERE email = $1", email)
-	if res.Scan() == sql.ErrNoRows {
-		fmt.Println("Doc Found")
-		exists <- true
+	var id int
+
+	err := res.Scan(&id, &prof.Name, &prof.Email, &prof.LinkedIn, &prof.Description, &prof.BirthDate, &prof.Key)
+	if err != nil {
+		fmt.Println(err.Error())
+		exists <- false
 		return
 	}
 
-	exists <- false
+	exists <- true
+	profileChan <- &prof
 
-	_, err := db.Exec("INSERT INTO profile (name, email, linkedin, description, birthdate) VALUES ($1, $2, $3, $4, $5)", name, email, linkedin, description, birthDate)
-	if err != nil {
-		fmt.Println(err.Error())
+	completeProfile := <-getMerged
+
+	var (
+		name        = completeProfile.Name
+		linkedin    = completeProfile.LinkedIn
+		description = completeProfile.Description
+		BirthDate   = completeProfile.BirthDate
+	)
+
+	_, err = db.Exec("UPDATE profile SET name = $1,linkedin = $2, description = $3, BirthDate = $4 WHERE id = $5", name, linkedin, description, BirthDate, id)
+}
+
+// Merges two profiles into one, Updates Second Profile with Firsts' Inputs
+func MergeProfiles(prof1 *statics.Profile, prof2 *statics.Profile) {
+	if prof1.Name != "" {
+		prof2.Name = prof1.Name
+	}
+	if prof1.Description != "" {
+		prof2.Description = prof1.Description
+	}
+	if prof1.LinkedIn != "" {
+		prof2.LinkedIn = prof1.LinkedIn
 	}
 }
